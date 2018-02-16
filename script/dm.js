2975d473000e3ec00b1c96944fdb4586eefb79bf
Mark Welker <marcopolovw@gmail.com>
2018-02-15 22:43:40 -0700

    Finished StatBlock generation jquery

--- script/dm.js
+++ script/dm.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 			creatures = json.results;
 		}
 	});
-	fetchCreatureByName("Orc");
+	fetchCreatureByName("Zombie");
 });
 
 function fetchCreatureByName(creatureName){
@@ -27,9 +27,31 @@ function fetchCreatureByURL(url){
 	$.ajax({
 		url : url,
 		dataType : "json",
-		success : function(json) {
-			console.log(json);
-			
+		success : function(creature) {
+
+			console.log(creature);
+			$('#c_name').text(creature.name);
+			$('#c_description').text(creature.size + " " + creature.type + " (" + creature.subtype + "), " + creature.alignment);
+			$('#c_ac').text("AC: " + creature.armor_class);
+			$('#c_hp').text("HP: " + creature.hit_points);
+			$('#c_str').text(creature.strength);
+			$('#c_dex').text(creature.dexterity);
+			$('#c_con').text(creature.constitution);
+			$('#c_int').text(creature.intelligence);
+			$('#c_wis').text(creature.wisdom);
+			$('#c_cha').text(creature.charisma);
+			var feature = "";
+			for (var i = 0; i < creature.special_abilities.length; i++){
+				feature += "<strong>" + creature.special_abilities[i].name + "</strong>: " + creature.special_abilities[i].desc;
+				feature += "<br />";
+			}
+			$('#c_features').html(feature);
+			var actions = "";
+			for (var i = 0; i < creature.actions.length; i++){
+				actions += "<strong>" + creature.actions[i].name + "</strong>: " + creature.actions[i].desc;
+				actions += "<br />";
+			}
+			$('#c_actions').html(actions);
 		}
 	});
 }
