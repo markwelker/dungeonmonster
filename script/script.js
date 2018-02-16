cdb3da3f1a7ebc792c914f6b51f327acf44f0320
Mark Welker <marcopolovw@gmail.com>
2018-02-15 22:00:29 -0700

    Merged into dm.html

--- script/script.js
+++ script/script.js
@@ -1,10 +1,1 @@
-$(document).ready(function() {
-	console.log("HELLLLLO WORLD!")
-	$.ajax({
-		url : "http://www.dnd5eapi.co/api/monsters/",
-		dataType : "json",
-		success : function(json) {
-			console.log(json);
-		}
-	});
-});
+
