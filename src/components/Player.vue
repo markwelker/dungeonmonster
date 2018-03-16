9ad3fa9b24ef132185aa7f838874bd63d9d5ee34
Vibroblade06 <rybro06@byu.edu>
2018-03-16 13:37:35 -0600

    Got store and node running

---
+++ src/components/Player.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="main-container-player" id="player-app">
+  <div class="party-container">
+    <h1 class="party-header">
+  				Party
+  				<img src="../assets/images/scroll.png" v-on:click="this.$router.push('spells')">
+  	</h1>
+    <PartyBar/>
+  </div>
+  <Chat/>
+  <div class="character-info">
+    <h1>Character Information</h1>
+    <div class="character-sheet">
+      <embed id="charsheet" src="../assets/pdf/Fighter.pdf"></embed>
+    </div>
+
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'Player',
+    data() {
+      return {
+        username: '',
+        
+      }
+    },
+  }
+</script>
