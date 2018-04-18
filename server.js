ac2a57dee2f5fe50f4f1e41092447a1ddeddca4a
Vibroblade06 <rybro06@byu.edu>
2018-04-18 01:29:09 -0600

    test code

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ app.get('/api/player/:name', (req, res) => {
   db('players').select().from('players').where('name', name).then(player => {
 		console.log('Found: "' + player + '" for "' + name + '"');
     var isDM = false;
-    if (player == null) res.status(403);
+    if (player == null || player === undefined || player === '') res.status(403);
     else {
 			console.log("creating token");
       let token = jwt.sign({
