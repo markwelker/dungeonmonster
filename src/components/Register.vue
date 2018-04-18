fc728e10d83768e5294c7ea47efcc27edcc2a257
Mark Welker <markwelker@byu.edu>
2018-04-17 19:58:16 -0600

    added ability to select class

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -16,10 +16,10 @@
 			<br />
 			Class
 			<br />
-			<img class="class-select" src="/static/images/Bard.png">
-			<img class="class-select" src="/static/images/Cleric.png">
-			<img class="class-select" src="/static/images/Fighter.png">
-			<img class="class-select" src="/static/images/Wizard.png">
+			<img class="class-select selected" v-on:click="select()" title="Bard" src="/static/images/Bard.png">
+			<img class="class-select" v-on:click="select()" title="Cleric" src="/static/images/Cleric.png">
+			<img class="class-select" v-on:click="select()" title="Fighter" src="/static/images/Fighter.png">
+			<img class="class-select" v-on:click="select()" title="Wizard" src="/static/images/Wizard.png">
 			<br />
 			<button class="login-button" v-on:click="register()">Register</button>
 		</div>
@@ -31,17 +31,22 @@
 export default {
 	name: 'Login',
 	data() {
-	return {
-		username: '',
-		password: '',
-	}
+		return {
+			username: '',
+			password: '',
+		}
 	},
 	methods: {
-	register: function() {
-		console.log("Registering Player");
-		var player = {name:this.username, password:this.password, class:'Fighter'};
-		this.$store.dispatch('registerPlayer', player);
-	},
+		register: function() {
+			console.log("Registering Player");
+			let playerClass = document.getElementsByClassName('selected').getAttribute('title');
+			var player = {name:this.username, password:this.password, playerClass};
+			this.$store.dispatch('registerPlayer', player);
+		},
+		select: function() {
+			document.getElementsByClassName('selected').classList.remove('selected');
+			this.classList.add('selected');
+		},
 	}
 }
 </script>
